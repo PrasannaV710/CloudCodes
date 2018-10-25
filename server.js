862e6895eb9275156f9810dad91d05c1be82879c
PrasannaV710 <39733996+PrasannaV710@users.noreply.github.com>
2018-10-25 15:34:37 +0530

    WeatherApp

--- server.js
+++ server.js
@@ -1,107 +1,37 @@
-'use strict';
-var express = require('express');
-var app = express();
-var path = require('path');
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost:27017/prasannadb";
-var port = process.env.PORT || 1337;
-var bodyParser = require('body-parser');
-var jsonParser = bodyParser.json();
-app.set('view engine','ejs');
-var message;
+const express = require('express');
+const bodyParser = require('body-parser');
+const request = require('request');
+const app = express()
+
+const apiKey = '7715405031adb9c2bf7dd752331de932';
+
 app.use(express.static('public'));
-
-
-// for parsing application/xwww-
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.use(express.static(path.join(__dirname, 'app')));
+app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-    res.render('index.ejs',{mess:message})
-});
-
-//Save – Insert 
-
-app.post('/save', function (req, res) {
-    console.log("Inside Save" + req.body.user);
-
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var myobj = { name: req.body.user, lname: req.body.ad, date:req.body.date };
-        var dbo=db.db("prasannadb");
-        dbo.collection("student").insertOne(myobj, function (err, res) {
-            if (err) throw err;
-            console.log("1 document inserted");
-            db.close();
-        });
-    });
-    res.send('File Inserted');
-});
-
-app.listen(1337, function () {
-    console.log('Example app listening on port 1337!')
+  res.render('index', {weather: null, error: null});
 })
 
-//Search
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
 
-app.post('/search', function (req, res) {
-    console.log("Inside Search" + req.body.user);
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees Celsius in ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+})
 
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-       // var query = { address: "Coimbatore" };
-        var query = { name : req.body.user }; // Get the user name from Form
-        var dbo=db.db("prasannadb");
-        dbo.collection("student").find(query).toArray(function (err, result) {
-        	var message1=result[0].name+" not found";
-            if (err) {
-            	res.render('index.ejs',{mess:message1});
-            }
-            console.log(result);
-            var message=result[0].name+" found!";
-            //res.send('Hello world in search' + result[0].name);
-            db.close();
-            res.render('index.ejs',{mess:message});
-        });
-    });
-   
-});
-
-//Update
-
-app.post('/update', function (req, res) {
-    console.log("Inside update" + req.body.user);
-
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var query = { name: req.body.user };
-        var newvalues = { name: req.body.f1name, lname: req.body.l1name, date: req.body.e1date };
-        var dbo=db.db("prasannadb");
-        dbo.collection("student").updateOne(query,{$set: newvalues}, function (err, res) {
-            if (err) throw err;
-            console.log("1 document updated");
-            db.close();
-        });
-    });
-   res.send('File Updated');
-});
-
-//Delete
-
-app.post('/delete', function (req, res) {
-    console.log("Inside delete" + req.body.user);
-
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        var query = { name: req.body.user };
-        var dbo=db.db("prasannadb");
-        dbo.collection("student").deleteOne(query, function (err, obj) {
-            if (err) throw err;
-            console.log("1 document deleted");
-            db.close();
-        });
-    });
-   res.send('File Deleted');
-});
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
